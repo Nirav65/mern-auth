13cc2334efeb5b1ed100cddbe823da1f8b7044da
Nirav Thakar <t.nirav888@gmail.com>
2024-05-16 17:53:20 -0500

    create dotenv, and added database connection to the API

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
-const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const port = 3000;
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Database connected successfully");
+}).catch((err) => {
+    console.log(err);
+})
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server running on ${port}`)
