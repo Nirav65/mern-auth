9ff64657389974113f118bddb2cd439d72802cd7
Nirav Thakar <t.nirav888@gmail.com>
2024-05-23 10:35:43 -0500

    signup page functionality using nodejs api

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,14 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/authController.js";
+import { signin, signup } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signup);
 
-router.get("/sign-in", (req, res) => {
-    res.json({
-        message: "Sign-in route added."
-    })
-});
+router.post("/sign-in", signin);
 
 export default router;
